3c08a07f892c7157cc90e2ac4395a32f5e0b9bfa
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2024-07-28 12:34:41 -0700

    reimplement touchscreen button clicking feature

--- Assets/Scripts/BoxClicked.cs
+++ Assets/Scripts/BoxClicked.cs
@@ -2,88 +2,105 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+
 //using System.Text.RegularExpressions;
 
 public class BoxClicked : MonoBehaviour
 {
-	GameManager gameManager;
-	GameObject smallGridHolder;
-	
+    GameManager gameManager;
+    bool clickStartedHere = false;
+    GameObject smallGridHolder;
 
-// Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-	    gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-	    smallGridHolder = GameObject.Find("Small Grids");
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        smallGridHolder = GameObject.Find("Small Grids");
     }
 
+    /// <summary>
+    /// This shit makes it so that the player can only play on a trigger where
+    /// they both start and end their click there without leaving the trigger
+    /// </summary>
 
-	/// <summary>
-	/// This shit makes it so that the player can only play on a trigger where 
-	/// they both start and end their click there without leaving the trigger
-	/// </summary>
+    private void OnMouseDown()
+    {
+        clickStartedHere = true;
+    }
 
+    private void OnMouseExit()
+    {
+        clickStartedHere = false;
+    }
 
     void OnMouseOver()
     {
-	    if(Input.GetMouseButtonDown(0))
-	    {
-		    UpdateTracking();
-        	
-		    //I don't think we still need to log this, do we?
-		    //Debug.Log(gameObject.name + gameObject.transform.parent.parent.name);
-	        
-		    SpawnXorO();
-	        
-		    GameObject.Find("WhereToPlay").GetComponent<WhereToPlay>().
-			    SetLocationAndScale((smallGridHolder.transform.GetChild(transform.GetSiblingIndex())).transform);
+        if (Input.GetMouseButtonUp(0) && clickStartedHere)
+        {
+            UpdateTracking();
+
+            //I don't think we still need to log this, do we?
+            //Debug.Log(gameObject.name + gameObject.transform.parent.parent.name);
+
+            SpawnXorO();
+
+            GameObject
+                .Find("WhereToPlay")
+                .GetComponent<WhereToPlay>()
+                .SetLocationAndScale(
+                    (smallGridHolder.transform.GetChild(transform.GetSiblingIndex())).transform
+                );
         }
     }
 
-	void VOnMouseOver()
+    void VOnMouseOver()
     {
-	    if(Input.GetButtonDown("Fire1"))
-	    {
-		    UpdateTracking();
-        	
-		    //I don't think we still need to log this, do we?
-		    //Debug.Log(gameObject.name + gameObject.transform.parent.parent.name);
-	        
-		    SpawnXorO();
-	        
-		    GameObject.Find("WhereToPlay").GetComponent<WhereToPlay>().
-			    SetLocationAndScale((smallGridHolder.transform.GetChild(transform.GetSiblingIndex())).transform);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            UpdateTracking();
+
+            //I don't think we still need to log this, do we?
+            //Debug.Log(gameObject.name + gameObject.transform.parent.parent.name);
+
+            SpawnXorO();
+
+            GameObject
+                .Find("WhereToPlay")
+                .GetComponent<WhereToPlay>()
+                .SetLocationAndScale(
+                    (smallGridHolder.transform.GetChild(transform.GetSiblingIndex())).transform
+                );
         }
     }
 
-	void SpawnXorO() // Instantiates a new O or X based on which player's turn it is and changes who's turn it is
-	{
-		GameObject newPiece = Instantiate(gameManager.prefabXO[Convert.ToInt32(gameManager.xPlayerTurn)]);
-		newPiece.transform.position = gameObject.transform.position;
+    void SpawnXorO() // Instantiates a new O or X based on which player's turn it is and changes who's turn it is
+    {
+        GameObject newPiece = Instantiate(
+            gameManager.prefabXO[Convert.ToInt32(gameManager.xPlayerTurn)]
+        );
+        newPiece.transform.position = gameObject.transform.position;
         newPiece.transform.parent = gameObject.transform.parent.parent;
-        
-		gameManager.xPlayerTurn = !gameManager.xPlayerTurn;
+
+        gameManager.xPlayerTurn = !gameManager.xPlayerTurn;
 
         gameObject.SetActive(false);
-	}
-    
-	void UpdateTracking() // tells the Grid Manager which thing was clicked
-	{
-		int intOfThis = transform.GetSiblingIndex();
-		
-		
-		
-		if (gameManager.xPlayerTurn)
-		{
-			transform.parent.parent.gameObject.GetComponent<GridManager>().
-				SetScoreOfThisThing(intOfThis,1);
-		}else
-		{
-			transform.parent.parent.gameObject.GetComponent<GridManager>().
-				SetScoreOfThisThing(intOfThis,-1);
-		}
-		
-	}
-    
-    
+    }
+
+    void UpdateTracking() // tells the Grid Manager which thing was clicked
+    {
+        int intOfThis = transform.GetSiblingIndex();
+
+        if (gameManager.xPlayerTurn)
+        {
+            transform.parent.parent.gameObject
+                .GetComponent<GridManager>()
+                .SetScoreOfThisThing(intOfThis, 1);
+        }
+        else
+        {
+            transform.parent.parent.gameObject
+                .GetComponent<GridManager>()
+                .SetScoreOfThisThing(intOfThis, -1);
+        }
+    }
 }
