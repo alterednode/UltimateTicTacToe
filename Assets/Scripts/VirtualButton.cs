f98760c09002d5ad5322533a6f69858c6a732a46
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2024-10-13 13:47:13 -0700

    fixed restart button issue

--- Assets/Scripts/VirtualButton.cs
+++ Assets/Scripts/VirtualButton.cs
@@ -1,20 +1,54 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class VirtualButton : MonoBehaviour
 {
     public Button buttonScript;
+    GameManager gameManager;
+    private VirtualMouse controller1;
+    private VirtualMouse controller2;
 
-    void Start(){
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        controller1 = GameObject.Find("Controller Cursor 1").GetComponent<VirtualMouse>();
+        controller2 = GameObject.Find("Controller Cursor 2").GetComponent<VirtualMouse>();
+
         buttonScript = GetComponent<Button>();
     }
 
-	void VOnMouseOver()
-	{
-    	
-    	
-		if (Input.GetButtonUp("Fire1") || Input.GetButtonUp("Fire1Alt"))
+    void VOnMouseOver()
+    {
+        // code pulled from BoxClicked.cs
+
+        bool xturn = gameManager.xPlayerTurn;
+
+        bool fireController1 = Input.GetButtonDown("Fire1");
+        bool fireController2 = Input.GetButtonDown("Fire1Alt");
+
+        bool canWeContinueTheGame = false;
+
+        if (controller1.inUse && controller2.inUse)
+        {
+            if (xturn && fireController1)
+            {
+                canWeContinueTheGame = true;
+            }
+            if ((!xturn) && fireController2)
+            {
+                canWeContinueTheGame = true;
+            }
+        }
+        if (controller1.inUse != controller2.inUse)
+        {
+            if (fireController1 || fireController2)
+            {
+                canWeContinueTheGame = true;
+            }
+        }
+
+        if (canWeContinueTheGame)
         {
             buttonScript.onClick.Invoke();
         }
--- Assets/Scripts/VirtualMouse.cs
+++ Assets/Scripts/VirtualMouse.cs
@@ -1,97 +1,96 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class VirtualMouse : MonoBehaviour {
+public class VirtualMouse : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed = 20f; // Speed of the cursor movement
+    public string horizAxisName;
+    public string vertAxisName;
+    public bool inUse = false;
+    GameManager gameManager;
+    GameObject cursorIndicator;
 
-	public bool isPlayer1; 
-	public float speed = 20f; // Speed of the cursor movement
-	public string horizAxisName;
-	public string vertAxisName;
-	public bool inUse = false;
-	GameManager gameManager;
-	GameObject cursorIndicator;
-	
     private Vector3 cursorPosition;
-	// Use this for initialization
-	void Start () {
-		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-		cursorPosition = transform.position;
-		cursorIndicator = GameObject.Find("CursorIndicator");
-		
-		//ensure this only happens once
-		if(isPlayer1)
-		{
-			cursorIndicator.transform.GetChild(0).gameObject.SetActive(false);
-			cursorIndicator.transform.GetChild(1).gameObject.SetActive(false);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Get input from the left stick
+
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        cursorPosition = transform.position;
+        cursorIndicator = GameObject.Find("CursorIndicator");
+
+        //ensure this only happens once
+        if (isPlayer1)
+        {
+            cursorIndicator.transform.GetChild(0).gameObject.SetActive(false);
+            cursorIndicator.transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from the left stick
         float horizontal = Input.GetAxis(horizAxisName);
-		float vertical = Input.GetAxis(vertAxisName);
-        
-        
-		// we check to see if the controller is used at all and if it is, we know that this controller is in use and make the cursor visible
-		if(horizontal!=0||vertical!=0) {inUse = true;}
-		transform.GetChild(0).gameObject.SetActive(inUse);
-		if(!inUse){return;}
+        float vertical = Input.GetAxis(vertAxisName);
+
+        // we check to see if the controller is used at all and if it is, we know that this controller is in use and make the cursor visible
+        if (horizontal != 0 || vertical != 0)
+        {
+            inUse = true;
+        }
+        transform.GetChild(0).gameObject.SetActive(inUse);
+        if (!inUse)
+        {
+            return;
+        }
 
         // Update the cursor position based on input
         cursorPosition += new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
 
-		Vector3 screenPos = Camera.main.WorldToScreenPoint(cursorPosition);
-
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(cursorPosition);
 
         // Clamp the cursor position to the screen bounds (optional)
-		//Debug.Log("Screen width: " + Screen.width);
+        //Debug.Log("Screen width: " + Screen.width);
         screenPos.x = Mathf.Clamp(screenPos.x, 0, Screen.width);
         screenPos.y = Mathf.Clamp(screenPos.y, 0, Screen.height);
 
-		cursorPosition = Camera.main.ScreenToWorldPoint(screenPos);
-
+        cursorPosition = Camera.main.ScreenToWorldPoint(screenPos);
 
         // Apply the new position to the cursor GameObject
-		transform.position = cursorPosition;
-        
-		if(gameManager.controller1.inUse&&gameManager.controller2.inUse)
-		{
-			//	Debug.Log("two controllers in use");
-			
-			
-			
-			
-			
-			
-			if(isPlayer1==gameManager.xPlayerTurn)
-			{	
-				cursorIndicator.transform.position=transform.position;
-				cursorIndicator.transform.GetChild(0).gameObject.SetActive(gameManager.xPlayerTurn);
-				cursorIndicator.transform.GetChild(1).gameObject.SetActive(!gameManager.xPlayerTurn);
-		
-			
-			}else
-			{
-				return;
-			}
-		}
-		if(gameManager.controller1.inUse!=gameManager.controller2.inUse)
-		{
-			cursorIndicator.transform.position=transform.position;
-			cursorIndicator.transform.GetChild(0).gameObject.SetActive(gameManager.xPlayerTurn);
-			cursorIndicator.transform.GetChild(1).gameObject.SetActive(!gameManager.xPlayerTurn);
-		}
-		
-		
-		
-		//	Debug.Log("sending Raycast, isPlayer1: " + isPlayer1);
-		RaycastHit hit = new RaycastHit();
-		Ray ray = new Ray(this.transform.position, Vector3.forward * 10);
-		Debug.DrawRay(this.transform.position, Vector3.forward * 10, Color.red);
+        transform.position = cursorPosition;
 
-		if (Physics.Raycast(ray, out hit, 1000.0f)){
-			hit.transform.SendMessage ("VOnMouseOver");
-		}
-	}
+        if (gameManager.controller1.inUse && gameManager.controller2.inUse)
+        {
+            //	Debug.Log("two controllers in use");
+
+            if (isPlayer1 == gameManager.xPlayerTurn)
+            {
+                cursorIndicator.transform.position = transform.position;
+                cursorIndicator.transform.GetChild(0).gameObject.SetActive(gameManager.xPlayerTurn);
+                cursorIndicator.transform.GetChild(1).gameObject.SetActive(!gameManager.xPlayerTurn);
+            }
+            else
+            {
+                return;
+            }
+        }
+        if (gameManager.controller1.inUse != gameManager.controller2.inUse)
+        {
+            cursorIndicator.transform.position = transform.position;
+            cursorIndicator.transform.GetChild(0).gameObject.SetActive(gameManager.xPlayerTurn);
+            cursorIndicator.transform.GetChild(1).gameObject.SetActive(!gameManager.xPlayerTurn);
+        }
+
+        //	Debug.Log("sending Raycast, isPlayer1: " + isPlayer1);
+        RaycastHit hit = new RaycastHit();
+        Ray ray = new Ray(this.transform.position, Vector3.forward * 10);
+        Debug.DrawRay(this.transform.position, Vector3.forward * 10, Color.red);
+
+        if (Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            hit.transform.SendMessage("VOnMouseOver");
+        }
+    }
 }
