a64f183d753945fe7883c4952acc6c0214e5bbc1
alterednode <75056546+alterednode@users.noreply.github.com>
2024-10-16 00:51:51 -0400

    added logging to the converter
    for debug purposes

--- Assets/Scripts/JsonConverter.cs
+++ Assets/Scripts/JsonConverter.cs
@@ -1,33 +1,62 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Text;
 
 public static class JsonConverter
 {
     // Converts a List<KeyValuePair<string, string>> to a JSON string
     public static string ListToJson(List<KeyValuePair<string, string>> list)
     {
-        return JsonUtility.ToJson(new Serialization<string, string>(list));
+        Debug.Log($"ListToJson: Input list count: {list.Count}");
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("{");
+        for (int i = 0; i < list.Count; i++)
+        {
+            sb.AppendFormat("\"{0}\":\"{1}\"", list[i].Key, list[i].Value);
+            if (i < list.Count - 1)
+                sb.Append(",");
+        }
+        sb.Append("}");
+
+        string result = sb.ToString();
+        Debug.Log($"ListToJson: Output JSON: {result}");
+        return result;
     }
 
     // Converts a JSON string to a List<KeyValuePair<string, string>>
     public static List<KeyValuePair<string, string>> JsonToList(string jsonString)
     {
-        return JsonUtility.FromJson<Serialization<string, string>>(jsonString).ToList();
+        Debug.Log($"JsonToList: Input JSON: {jsonString}");
+
+        var list = new List<KeyValuePair<string, string>>();
+
+        try
+        {
+            var wrapper = JsonUtility.FromJson<Wrapper>("{\"Items\":" + jsonString + "}");
+            Debug.Log($"JsonToList: Wrapper parsed successfully. Item count: {wrapper.Items.Count}");
+
+            foreach (var kvp in wrapper.Items)
+            {
+                list.Add(new KeyValuePair<string, string>(kvp.Key, kvp.Value));
+                Debug.Log($"JsonToList: Added pair - Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"JsonToList: Error parsing JSON: {e.Message}");
+            Debug.LogError($"JsonToList: Stack trace: {e.StackTrace}");
+        }
+
+        Debug.Log($"JsonToList: Output list count: {list.Count}");
+        return list;
     }
 
-    [System.Serializable]
-    private class Serialization<TKey, TValue>
+    // Helper class to deserialize JSON
+    [Serializable]
+    private class Wrapper
     {
-        public List<KeyValuePair<TKey, TValue>> keyValuePairs;
-
-        public Serialization(List<KeyValuePair<TKey, TValue>> list)
-        {
-            keyValuePairs = new List<KeyValuePair<TKey, TValue>>(list);
-        }
-
-        public List<KeyValuePair<TKey, TValue>> ToList()
-        {
-            return new List<KeyValuePair<TKey, TValue>>(keyValuePairs);
-        }
+        public Dictionary<string, string> Items;
     }
 }
