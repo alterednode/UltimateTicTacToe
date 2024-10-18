be7c63c4df3a57f68848c6657cf145cbd4d47477
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2024-10-17 19:05:57 -0700

    main menu controller support

--- Assets/Scripts/VirtualButton.cs
+++ Assets/Scripts/VirtualButton.cs
@@ -5,30 +5,19 @@ public class VirtualButton : MonoBehaviour
 {
     public Button buttonScript;
     GameManager gameManager;
-    private VirtualMouse controller1;
-    private VirtualMouse controller2;
+    ControllerManager controllerManager;
 
     public bool canAlwaysBeClicked = false;
 
     void Start()
     {
+        controllerManager = GameObject.Find("ControllerManager").GetComponent<ControllerManager>();
         try
         {
 
             if (!canAlwaysBeClicked)
             {
                 gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-            }
-            //TODO: make controllers work on main menu
-            try
-            {
-                controller1 = GameObject.Find("Controller Cursor 1").GetComponent<VirtualMouse>();
-                controller2 = GameObject.Find("Controller Cursor 2").GetComponent<VirtualMouse>();
-
-            }
-            catch (System.Exception)
-            {
-                Debug.LogWarning("remember to make controllers work on main menu");
             }
         }
         catch
@@ -55,7 +44,7 @@ public class VirtualButton : MonoBehaviour
 
             bool canWeContinueTheGame = false;
 
-            if (controller1.inUse && controller2.inUse)
+            if (controllerManager.controller1.inUse && controllerManager.controller2.inUse)
             {
                 if (xturn && fireController1)
                 {
@@ -66,7 +55,7 @@ public class VirtualButton : MonoBehaviour
                     canWeContinueTheGame = true;
                 }
             }
-            if (controller1.inUse != controller2.inUse)
+            if (controllerManager.controller1.inUse != controllerManager.controller2.inUse)
             {
                 if (fireController1 || fireController2)
                 {
